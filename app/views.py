d5a916daca8dd4b8c4746f7c7d9267d5ffaea329
Benedict Solomon <solomon103098@gmail.com>
2020-10-15 17:46:33 +0800

    added register ui and password confirmation

--- app/views.py
+++ app/views.py
@@ -1,6 +1,32 @@
 from app import *
 from app.controllers import *
 
-@server.route("/")
+@server.route("/", methods=["GET"])
 def index():
 	return render_template('index.html')
+
+@server.route("/signup", methods=["GET", "POST"])
+def signup():
+	messege = ""
+	form_dict = {
+		"first_name" : "",
+		"last_name" : "",
+		"email" : ""
+	}
+	if request.method == "POST":
+		if request.form["confirm_password"] == request.form["password"]:
+			print(request.form["password"])
+			return redirect(url_for('home'))
+		else:
+			form_dict["first_name"] = request.form["first_name"]
+			form_dict["last_name"] = request.form["last_name"]
+			form_dict["email"] = request.form["email"]
+
+			messege = "Password does not match"
+			print(messege)
+			return render_template("auth/signup.html", messege=messege, form_dict= form_dict)
+	return render_template("auth/signup.html", messege=messege, form_dict= form_dict)
+
+@server.route("/home", methods=["GET", "POST"])
+def home():
+	return "Hiiii!!!!!"
